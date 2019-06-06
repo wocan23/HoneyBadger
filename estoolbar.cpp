94210b27b87cfd1430cfee1522c278ec37f9d85e
zhaoshuai3 <1115870494@qq.com>
2019-06-06 16:05:45 +0800

    帮助

--- estoolbar.cpp
+++ estoolbar.cpp
@@ -3,20 +3,21 @@
 
 EsToolBar::EsToolBar(QWidget *parent) : QToolBar(parent)
 {
-    QAction *act[5];
+    QAction *act[6];
 
       act[0] = new QAction(QIcon(":/icon/pic/conn.png"), "conn", this);
       act[1] = new QAction(QIcon(":/icon/pic/index.png"), "index", this);
       act[2] = new QAction(QIcon(":/icon/pic/doc.png"), "doc", this);
       act[3] = new QAction(QIcon(":/icon/pic/editDoc.png"), "edit", this);
       act[4] = new QAction(QIcon(":/icon/pic/search.png"), "search", this);
+      act[5] = new QAction(QIcon(":/icon/pic/help.png"), "help", this);
 
       act[0]->setShortcut(Qt::Key_Control | Qt::Key_E);
       act[0]->setToolTip("This is a ToolTip");
 
       this->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
 
-      for(int i = 0 ; i < 5 ; i++)
+      for(int i = 0 ; i < 6 ; i++)
       {
           this->addAction(act[i]);
       }
