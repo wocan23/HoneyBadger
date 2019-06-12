a385f459e0e5b8cdd44ee9244532f090f6a541ee
zhaoshuai3 <1115870494@qq.com>
2019-06-12 17:23:07 +0800

    增加QSplitter

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSplitter>
+#include <QScrollArea>
 #include "estoolbar.h"
 #include <QDebug>
-#include "estreewidgetitem.h"
 #include <QStringList>
 #include "httputils.h"
 #include "conn.h"
 #include "esindextabwidegt.h"
+#include "estreewidget.h"
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -17,10 +19,50 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     this->esToolBar = new EsToolBar;
     this->ui->toolBarLayout->addWidget(this->esToolBar);
-//    this->ui->mainLayout.a
-    this->ui->treeWidget->setHeaderHidden(true);
-    EsIndexTabWidegt *tabWidget = new EsIndexTabWidegt;
-    this->ui->scrollArea->setWidget(tabWidget);
+
+
+    EsTreeWidget * treeWidget = new EsTreeWidget;
+    QSizePolicy treePolicy = treeWidget->sizePolicy();
+    treePolicy.setHorizontalStretch(2);
+    treeWidget->setSizePolicy(treePolicy);
+    treeWidget->setMinimumWidth(60);
+    treeWidget->setMaximumWidth(600);
+
+
+    treeWidget->setHeaderHidden(true);
+    EsIndexTabWidegt *tab = new EsIndexTabWidegt;
+    tab->setVisible(true);
+    //    tab->setMaximumWidth(20);
+    tab->setAutoFillBackground(true);
+//    tab->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+//    tab->setVisible(true);
+//    tab->setTabsClosable(true);
+
+
+    QSplitter * splitter = new QSplitter(Qt::Horizontal);
+
+    QScrollArea * area = new QScrollArea;
+    area->setWidget(tab);
+    area->setWidgetResizable(true);
+    area->setAutoFillBackground(true);
+    area->setMinimumWidth(300);
+    area->setSizePolicy(QSizePolicy::Maximum,QSizePolicy::Preferred);
+    area->setVisible(true);
+
+//    QSizePolicy areaP = area->sizePolicy();
+//    areaP.setHorizontalStretch(9);
+//    area->setSizePolicy(areaP);
+//    area->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+
+//    splitter->setStretchFactor(0,1);
+//    splitter->setStretchFactor(1,7);
+
+    splitter->addWidget(treeWidget);
+    splitter->addWidget(area);
+    splitter->setCollapsible(0,true);
+
+
+    this->ui->verticalLayout->addWidget(splitter);
 
 
 
